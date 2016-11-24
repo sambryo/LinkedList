4f1b21f199c11cd045dbd0e85e3ccc5ee7e9d296
Samuel <samuelbryo@gmail.com>
2016-11-23 18:15:46 -0800

    display method

--- LinkedList.java
+++ LinkedList.java
@@ -8,6 +8,7 @@ public class LinkedList {
 
     public LinkedList (Node n){
         head = n;
+        //head.next = null;
     }
     public static void main (String[] args ){
         Node newNode = new Node (3);
@@ -21,22 +22,82 @@ public class LinkedList {
         LinkedList h = new LinkedList(newNode);
 
         System.out.println(display(newNode));
+        System.out.println(display2(newNode));
+
+        int[] newArray = {6,5,2,1,8,9,2,7};
+        LinkedList ll2 = makeLinkedListFromArray(newArray);
+        System.out.println(displayLinkedList(ll2));
+
+
 
     }
+    public static String displayLinkedList(LinkedList l){
+        StringBuffer str = new StringBuffer();
+        while (l.head.next != null){
+            str.append(l.head.data);
+            str.append("--->");
+            l.head = l.head.next;
 
-    public static String display (Node head) {
+        }
+        str.append(l.head.data);
+        return str.toString();
+    }
 
-        String result  = " ";
+     public static String display (Node head) {
+
+        String result = "";
         while (head.next != null){
-            result.concat(Integer.toString(head.data));
-            result.concat(" -> ");
+           result = result.concat(Integer.toString(head.data));
+           result = result.concat(" -> ");
 
             head = head.next;
         }
-        result += Integer.toString(head.data);
+        result = result.concat(Integer.toString(head.data));
 
         return result;
 
+    }
+
+
+    public static String display2 (Node head) {
+
+        StringBuffer result  = new StringBuffer();
+        while (head.next != null){
+            result.append(head.data);
+            result.append(" -> ");
+
+            head = head.next;
+        }
+        result.append(head.data);
+
+        return result.toString();
+
+    }
+
+    public static LinkedList makeLinkedListFromArray (int[] array){
+        if (array.length == 0)
+            return null;
+        Node head = new Node (array[0]);
+        Node headCopy = head;
+        for (int i=1; i<array.length; i++){
+            head.next = new Node(array[i]);
+            head = head.next;
+        }
+        head.next = null;
+        return new LinkedList(headCopy);
+
+
+    }
+
+    public static Node reverseAndCopy(Node head){
+        Node n = null;
+        while (head != null){
+            Node r = new Node (head.data); // clone
+            r.next = n;
+            n = head;
+            head = head.next;
+        }
+        return n;
     }
 }
 class Node{
